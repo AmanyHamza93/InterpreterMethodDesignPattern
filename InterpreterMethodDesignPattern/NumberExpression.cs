7172ab9b28b5c076bad21423d90d17497cfb0597
a.hamza <a.hamza@plugin-soft.com>
2024-08-31 11:25:15 +0300

    Add project files.

---
+++ InterpreterMethodDesignPattern/NumberExpression.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterpreterMethodDesignPattern
+{
+    public class NumberExpression : Expression
+    {
+        private int number;
+
+        public NumberExpression(int number)
+        {
+            this.number = number;
+        }
+
+        public override int Interpret()
+        {
+            return number;
+        }
+    }
+}
---
+++ InterpreterMethodDesignPattern/Program.cs
@@ -0,0 +1,8 @@
+﻿// Creating expressions: 1 + 2
+using InterpreterMethodDesignPattern;
+
+Expression expression = new AdditionExpression(new NumberExpression(1), new NumberExpression(2));
+
+// Interpreting and evaluating the expression
+int result = expression.Interpret();
+Console.WriteLine("Result:" + result); // Output: 3
